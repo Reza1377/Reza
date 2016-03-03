e1477a9755b01d67b26e6e28fe5830f805f96a63
kineh1bot <ali.fathi313.af@gmail.com>
2016-03-03 21:57:38 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
 local bot_id =  110311925
-local fbotmain = 187232373,153862670,186625098
+local fbotmain = 187232373
     if matches[1]:lower == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
