0a4620a45996c6d572c54572ee12c4493d342d27
kineh1bot <ali.fathi313.af@gmail.com>
2016-03-04 15:35:05 +0330

    Update st.lua

--- plugins/st.lua
+++ plugins/st.lua
@@ -2,12 +2,9 @@ local function run(msg)
  if not is_sudo(msg) then
               return "siktir baw"
        end
-if msg.text == "عشقم" then
-	return "بله عزیزم ؟"
-if msg.text == "نوس اسپید" then
-	return "بله ارباب😍 ؟"
 if msg.text == "امیر" then
-	return "با باباییم چیکار داری😎 ؟"
+	return "با باباییم چیکار داری"
+
 end
 end
 
