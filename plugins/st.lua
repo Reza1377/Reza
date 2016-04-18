7a0b6b62212d68f3fd6082ab6d3bbc7639988aff
Reza1377 <elmoadabh88@gmail.com>
2016-04-18 22:57:11 +0430

    Update st.lua

--- plugins/st.lua
+++ plugins/st.lua
@@ -2,7 +2,7 @@ local function run(msg)
  if not is_sudo(msg) then
               return "خفشو تو امیر نیستی جنده لاشی😡"
        end
-if msg.text == "نوس اسپید" then
+if msg.text == "بوت مستر" then
 	return "جوووووونمممممم باااااابااااااااییییییی😍😍"
 
 end
@@ -12,7 +12,7 @@ return {
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
-	       	"^(نوس اسپید)$",
+	       	"^(بوت مستر)$",
 	       			}, 
 	run = run,
     --privileged = true,
